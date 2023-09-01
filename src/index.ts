a6a2b162184429932bc9a93651bcaae4076f83e9
Mario Gallaun <mario.gallaun@gmx.at>
2023-09-01 10:35:31 +0200

    Add weather.com Sink

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import config from 'config';
-import logger from "./modules/logger";
-import davisIpCollector, {DavisResponseData} from "./modules/davisIpCollector";
-import InfluxDbSink from "./modules/influxDbSink";
-import WeatherCloudSink from "./modules/weatherCloudSink";
-import WeatherUndergroundSink from "./modules/weatherUndergroundSink";
+import config from 'config'
+import logger from "./modules/logger"
+import davisIpCollector, {DavisResponseData} from "./modules/davisIpCollector"
+import InfluxDbSink from "./modules/influxDbSink"
+import WeatherCloudSink from "./modules/weatherCloudSink"
+import WeatherUndergroundSink from "./modules/weatherUndergroundSink"
+import WeatherComSink from "./modules/weatherComSink"
 
 logger.info("Application Startup....",  { component: "main" })
 
@@ -11,6 +12,7 @@ const davisIP = new davisIpCollector(config.get('davisIP.ip'), config.get('davis
 const influxDbSink = new InfluxDbSink(config.get('uploadServices.influxdb.apiUrl'), config.get('uploadServices.influxdb.timeout'), config.get('uploadServices.influxdb.uploadInterval'))
 const weatherCloudSink = new WeatherCloudSink(config.get('uploadServices.weathercloud.apiUrl'), config.get('uploadServices.weathercloud.user'), config.get('uploadServices.weathercloud.password'), config.get('uploadServices.weathercloud.timeout'), config.get('uploadServices.weathercloud.uploadInterval'))
 const weatherUndergroundSink = new WeatherUndergroundSink(config.get('uploadServices.weatherunderground.apiUrl'), config.get('uploadServices.weatherunderground.user'), config.get('uploadServices.weatherunderground.password'), config.get('uploadServices.weatherunderground.timeout'), config.get('uploadServices.weatherunderground.uploadInterval'))
+const weatherComSink = new WeatherComSink(config.get('uploadServices.wettercom.apiUrl'), config.get('uploadServices.wettercom.user'), config.get('uploadServices.wettercom.password'), config.get('uploadServices.wettercom.timeout'), config.get('uploadServices.wettercom.uploadInterval'))
 
 setInterval(getDataFromWeatherStation, 10000);
 
@@ -20,6 +22,7 @@ function getDataFromWeatherStation() {
         influxDbSink.tryPush(data)
         weatherCloudSink.tryPush(data)
         weatherUndergroundSink.tryPush(data)
+        weatherComSink.tryPush(data)
     }).catch((error) => {
         logger.error(`Something went wrong while fetching data from weather-station. Error: ${error}`, { component: "main" })
     })
---
+++ src/modules/weatherComSink.ts
@@ -0,0 +1,82 @@
+import logger from './logger'
+import axios from "axios";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"
+import {DavisResponseData} from "./davisIpCollector";
+import * as UnitConversion from "./unitConverstion"
+
+class WeatherComSink {
+    #user: string
+    #password: string
+    #apiUrl: string
+    #timeout: number
+    #uploadInterval: number
+    #lastUpload: Date = new Date()
+
+    constructor(apiUrl: string, user: string, password: string, timeout: number, interval: number) {
+        this.#apiUrl = apiUrl
+        this.#user = user
+        this.#password = password
+        this.#timeout = timeout
+        this.#uploadInterval = interval
+
+        dayjs.extend(utc)
+    }
+
+    tryPush(data: DavisResponseData) {
+        if((Date.now() - this.#lastUpload.getTime()) < this.#uploadInterval * 1000) {
+            logger.debug(`Nothing to do. Last upload was ${this.#lastUpload}`, { component: 'WeatherComSink'})
+            return
+        }
+
+        axios.get(this.#apiUrl, {timeout: this.#timeout, params: this.buildQueryString(data)}).then((resp) => {
+            logger.info('Data successfully pushed to WeatherCom.', { component: 'WeatherComSink'})
+            logger.debug(`Response Data: ${resp.data}`, { component: 'WeatherComSink'})
+        }).catch((error) => {
+            logger.error(`Error pushing data to WeatherCom. Error: ${error}`, { component: 'WeatherComSink'})
+        })
+
+        this.#lastUpload = new Date();
+    }
+
+    private buildQueryString(data: DavisResponseData) {
+        return {
+            'test': false,
+            'id': this.#user,
+            'pwd': this.#password,
+            'sid': 'weewx',
+            'version': '3.0',
+            'dtutc': dayjs().utc().format('YYYYYMMDDHHmm'),
+            'hu': data.OutHum,
+            'te': data.OutTemp,
+            'dp': data.DewPoint,
+            'pr': data.Pressure,
+            'pcf': this.formatPressureTrend(data.BarTrend),
+            'wd': data.WindDir,
+            'ws': UnitConversion.Kmhtoms(data.WindSpeed2av),
+            'wg': UnitConversion.Kmhtoms(data.WindGust),
+            'pa': data.RainLast1h,
+            'rr': data.RainRate
+        }
+    }
+
+    private formatPressureTrend(pressure: number): number {
+        switch (pressure) {
+            case -60:
+            case -20:
+                return 7;
+                break;
+            case 0:
+                return 4;
+                break;
+            case 20:
+            case 60:
+                return 2;
+                break;
+            default:
+                return 4
+        }
+    }
+}
+
+export default WeatherComSink
