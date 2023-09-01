5c631ab59e84cee12b60c2bca2f959557fa77932
Mario Gallaun <mario.gallaun@gmx.at>
2023-09-01 10:11:10 +0200

    Add Weather Underground Sink

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import logger from "./modules/logger";
 import davisIpCollector, {DavisResponseData} from "./modules/davisIpCollector";
 import InfluxDbSink from "./modules/influxDbSink";
 import WeatherCloudSink from "./modules/weatherCloudSink";
+import WeatherUndergroundSink from "./modules/weatherUndergroundSink";
 
 logger.info("Application Startup....",  { component: "main" })
 
 const davisIP = new davisIpCollector(config.get('davisIP.ip'), config.get('davisIP.port'), config.get('davisIP.timeout'))
 const influxDbSink = new InfluxDbSink(config.get('uploadServices.influxdb.apiUrl'), config.get('uploadServices.influxdb.timeout'), config.get('uploadServices.influxdb.uploadInterval'))
 const weatherCloudSink = new WeatherCloudSink(config.get('uploadServices.weathercloud.apiUrl'), config.get('uploadServices.weathercloud.user'), config.get('uploadServices.weathercloud.password'), config.get('uploadServices.weathercloud.timeout'), config.get('uploadServices.weathercloud.uploadInterval'))
+const weatherUndergroundSink = new WeatherUndergroundSink(config.get('uploadServices.weatherunderground.apiUrl'), config.get('uploadServices.weatherunderground.user'), config.get('uploadServices.weatherunderground.password'), config.get('uploadServices.weatherunderground.timeout'), config.get('uploadServices.weatherunderground.uploadInterval'))
 
 setInterval(getDataFromWeatherStation, 10000);
 
@@ -17,6 +19,7 @@ function getDataFromWeatherStation() {
         logger.debug(`Received data from weather-station: Data: ${JSON.stringify(data)}`, { component: "main" })
         influxDbSink.tryPush(data)
         weatherCloudSink.tryPush(data)
+        weatherUndergroundSink.tryPush(data)
     }).catch((error) => {
         logger.error(`Something went wrong while fetching data from weather-station. Error: ${error}`, { component: "main" })
     })
---
+++ src/modules/weatherUndergroundSink.ts
@@ -0,0 +1,62 @@
+import logger from './logger'
+import axios from "axios";
+import dayjs from "dayjs";
+import * as UnitConversion from "./unitConverstion"
+import {DavisResponseData} from "./davisIpCollector";
+
+class WeatherUndergroundSink {
+    #user: string
+    #password: string
+    #apiUrl: string
+    #timeout: number
+    #uploadInterval: number
+    #lastUpload: Date = new Date()
+
+    constructor(apiUrl: string, user: string, password: string, timeout: number, interval: number) {
+        this.#apiUrl = apiUrl
+        this.#user = user
+        this.#password = password
+        this.#timeout = timeout
+        this.#uploadInterval = interval
+    }
+
+    tryPush(data: DavisResponseData) {
+        if((Date.now() - this.#lastUpload.getTime()) < this.#uploadInterval * 1000) {
+            logger.debug(`Nothing to do. Last upload was ${this.#lastUpload}`, { component: 'WeatherUndergroundSink'})
+            return
+        }
+
+        axios.get(this.#apiUrl, {timeout: this.#timeout, params: this.buildQueryString(data)}).then((resp) => {
+            logger.info('Data successfully pushed to WeatherUnderground.', { component: 'WeatherUndergroundSink'})
+            logger.debug(`Response Data: ${resp.data}`, { component: 'WeatherUndergroundSink'})
+        }).catch((error) => {
+            logger.error(`Error pushing data to WeatherUnderground. Error: ${error}`, { component: 'WeatherUndergroundSink'})
+        })
+
+        this.#lastUpload = new Date();
+    }
+
+    private buildQueryString(data: DavisResponseData) {
+        return {
+            'action': 'updateraw',
+            'ID': this.#user,
+            'PASSWORD': this.#password,
+            'dateutc': dayjs().format('YYYY-MM-DD HH:mm:ss'),
+            'winddir': data.WindDir,
+            'windspeedmph': UnitConversion.KmhtoMph(data.WindSpeed),
+            'windgustmph': UnitConversion.KmhtoMph(data.WindGust),
+            'windgustdir': data.WindGustDir,
+            'windspdmph_avg2m': UnitConversion.KmhtoMph(data.WindSpeed2av),
+            'humidity': data.OutHum,
+            'dewptf': UnitConversion.CtoF(data.DewPoint),
+            'tempf': UnitConversion.CtoF(data.OutTemp),
+            'rainin': UnitConversion.MmtoInch(data.RainRate),
+            'dailyrainin': UnitConversion.MmtoInch(data.RainDay),
+            'baromin': UnitConversion.MmtoInch(data.Pressure),
+            'realtime': 1,
+            'rtfreq': 10
+        }
+    }
+}
+
+export default WeatherUndergroundSink
